2e3b134860528bdcccff1612f607d1956e31f3f3
sammy23322 <savroop2812@gmail.com>
2026-05-23 00:10:51 +0530

    authentication to update the todo by user id

--- TodoApp/routers/todos.py
+++ TodoApp/routers/todos.py
@@ -62,10 +62,14 @@ async def create_new_todo(user: user_dependency, db : db_dependency, todo_reques
 
 @router.put("/todo/{todo_id}", status_code= status.HTTP_204_NO_CONTENT)
 async def update_todo(db:db_dependency,
+                      user: user_dependency,
                        todo_request: TodoRequest,
                        todo_id : int = Path(gt = 0)):
+    if user is None:
+         raise HTTPException(status_code=401 , detail='Authentication Failed')
+    
      
-    todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+    todo_model = db.query(Todos).filter(Todos.id == todo_id).filter(Todos.owner_id == user.get('id')).first()
     if todo_model is  None:
         raise HTTPException(status_code = 404 , detail="Todo not found")
 
