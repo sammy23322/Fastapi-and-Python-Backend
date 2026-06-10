0cac03269007ed627cefbc8a39e188b438627b6b
sammy23322 <savroop2812@gmail.com>
2026-06-10 23:27:13 +0530

    users endpoint to change password and user_model

--- TodoApp/routers/auth.py
+++ TodoApp/routers/auth.py
@@ -65,17 +65,17 @@ def authenticate_user(username : str ,password :str, db:db_dependency):
 async def get_current_user(token : Annotated[str, Depends(oauth2bearer)]):
     try:
         payload = jwt.decode(token, SECRET_KEY,algorithms= [ALGORITHM])
-        username = payload.get("sub")
-        user_id = payload.get('id')
-        user_role = payload.get('role')
+        username: str = payload.get("sub")
+        user_id: int = payload.get('id')
+        user_role: str = payload.get('role')
         if username is None  or user_id is None:
             raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
         return {'username': username ,'id': user_id, 'user_role':user_role }
     except JWTError:                                                        #what is JWT Error ? why it is used ? 
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
 
-def create_access_token(username :str , user_id : int , role: str,expires_delta : timedelta):
-    encode = {'sub' : username , 'id' : user_id,role:role }
+def create_access_token(username :str , user_id : int , role: str, expires_delta : timedelta):
+    encode = {'sub' : username , 'id' : user_id, role : role }
     expires = datetime.datetime.now() + expires_delta              #didnt understood this line 
     encode.update({'exp': expires})
     return jwt.encode(encode , SECRET_KEY , algorithm=ALGORITHM)
@@ -103,5 +103,5 @@ async def login_for_access_token(form_data : Annotated[OAuth2PasswordRequestForm
 
     if not user:
         raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED) 
-    token = create_access_token(user.user_name, user.id,user.role,  timedelta(minutes = 20))
+    token = create_access_token(user.user_name , user.id , user.role,  timedelta(minutes = 20))
     return {'access_token' : token , 'token_type' : 'bearer'}
---
+++ TodoApp/routers/users.py
@@ -0,0 +1,54 @@
+from fastapi import APIRouter, Depends, HTTPException , status, Path, Field
+from models import users
+from pydantic import BaseModel
+from database import SessionLocal
+from typing import Annotated
+from sqlalchemy.orm import Session
+from models import Todos, Users
+from routers.auth import get_current_user
+from passlib.context import CryptContext
+from fastapi.security import OAuth2PasswordRequestForm ,OAuth2PasswordBearer 
+
+router = APIRouter(
+    prefix = "/user",
+    tags = ["user"]
+)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+# why a class is made and what is the possible solution without classes
+class Usererfication(BaseModel):         
+    password:str
+    new_password:str = Field(min_length= 6)
+
+
+#dont know the meaning of this line entirely
+bcrypt_context = CryptContext(schemes=['bcrypt'] , deprecated = 'auto')
+oauth2bearer = OAuth2PasswordBearer(tokenUrl = "auth/token")
+db_dependency = Annotated[Session , Depends(get_db)]
+user_dependency = Annotated[dict, Depends(get_current_user)]
+
+
+@router.get("/details/{user_id}}",  status_code=status.HTTP_200_OK)
+async def get_user(user:user_dependency, db:db_dependency):
+    if user is None:
+        raise HTTPException(status_code=401 , detail='Authentication Failed')
+    
+    return db.query(Users).filter(Users.id == user.get('id'))
+
+@router.post("/password", status_code=status.HTTP_204_NO_CONTENT)
+async def change_password(user:user_dependency, db:db_dependency,
+                          user_verification:Usererfication):
+    if user is None:
+        raise HTTPException(status_code=401, detail = "authentication failed")
+    user_model = db.query(Users).filter(Users.id == user.get('id')).first()
+
+    if not bcrypt_context.verify(user_verification.password, user_model.hashed_pasword):#doubt in this line 
+        raise HTTPException(status_code = 401 , detail = "error on password change")
+    user_model.hashed_password = bcrypt_context.hash(user_verification.new_password)
+    db.add(user_model)
+    db.commit()
